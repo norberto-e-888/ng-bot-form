a60305692c6b43fe5392bef3a8c77bc80fab9419
Norberto Cáceres <norberto.e.888@outlook.es>
2021-02-17 16:33:36 -0500

    <fix> try exporting the component as well from public-api

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,4 +1,5 @@
 export { BotFormEffects } from './lib/redux/effects';
 export { BotFormModule } from './lib/bot-form.module';
+export { ChatbotComponent } from './lib/chat/chat.component';
 export * from './lib/typings';
 export * from './lib/redux';
